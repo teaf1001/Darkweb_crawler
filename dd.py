293607ac711cb2d35eca7acd65ba19907c269340
teaf1001 <teaf1001@naver.com>
2021-01-15 21:13:45 +0900

    update

--- dd.py
+++ dd.py
@@ -14,12 +14,7 @@ proxies = {
 
 
 class crawler:
- '''
-    def __init__(self):
-        self.url = ''
-        self.res = requests.get(self.url, proxies=proxies)
-'''
-
+        
     def web_parser(url, res):
         
         #parse html code
@@ -45,12 +40,71 @@ class crawler:
 
         return res
 
-    #def get_url(self):
+    def get_url(url):
+        #html code parse
+        res = requests.get(url)
+        html = res.text
+        parse = BeautifulSoup(html, 'html.parser')
+
+        #<a> tag parse
+        links= parse.find_all("a")
+
+        #select url
+        for tag in links:
+            href = tag.attrs['href']
+
+            if str(href)[0] != 'h':
+                if "#" in href :
+                    continue
+                elif str(href)[0] == '/' :
+                    href = url + str(href)
+                else :
+                    href = url + '/' + str(href)
+            
+            if len(href) <= 4 :
+                continue
+            if href[-1] == '/':
+                href = href[0:-1]
+            
+            #save url in the html code
+            print(href)
+            f = open("./test/test.txt", "a+")
+            f.write(href)
+            f.write("\n")
+            f.close()
+
+    #def find_url(path):
 
 if __name__=="__main__":
-    url = 'https://nate.com'
-    res = crawler.isurl_exist(url)
+    url = 'https://naver.com'
 
+    #crawler.get_url(url)
+
+    path = './test/test.txt'
+
+    f = open(path)
+
+    urls = f.readlines()
+
+    for i in range(0, len(urls)):
+        if urls[i] != '':
+            try:
+                print(urls[i], ': ', requests.get(urls[i]))
+            except:
+                pass
+    #print(urls[0], ': ', requests.get(urls[0]))
+
+
+
+
+
+
+'''
     if res.status_code == 200:
         print("hi")
         crawler.web_parser(url, res)
+    '''
+
+''' def __init__(self):
+self.url = ''
+self.res = requests.get(self.url, proxies=proxies)'''
