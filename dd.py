e5f8d6361900c2f578f4c4d50f769307dcb6b951
teaf1001 <teaf1001@naver.com>
2021-01-22 17:18:23 +0900

    url collector finish from one page

--- dd.py
+++ dd.py
@@ -1,13 +1,9 @@
 import requests
-import socks
-import socket
 import os
 from bs4 import BeautifulSoup
 import datetime
 import socket
-#import getmac
 import tldextract as tld
-#from urllib.request import urlopen
 import time
 
 proxies = {
@@ -15,89 +11,77 @@ proxies = {
     'https': 'socks5h://127.0.0.1:9150'
 }
 
-class crawler:
-
-    def isurl_exist(url):
-        extract = tld.extract(url)
-        domain = "http://{}.{}".format(extract.domain, extract.suffix)
-
-        try:
-            res = requests.get(domain, proxies=proxies, timeout=15)
-        except:
-            print("EXCEPT PASS : ", domain)
-            pass
-
-        return res
-
-    def web_parser(url, res):
-        # 수정하기 : 파일 이름 도메인 + 서픽스로 하지 말고 http://만 빼고 다 작성하기 -> 모든 페이지 대상
-        # save html code as a file
-        name = url.replace('https://','').replace('http://','').replace('/', '')
-        #path = "{}.{}".format(extract.domain, extract.suffix)
-        path = os.getcwd() + '\\web\\' + name
-        f = open(path, 'a+', encoding='utf8')
+def web_parser(url, res):
+    # 수정하기 : 파일 이름 도메인 + 서픽스로 하지 말고 http://만 빼고 다 작성하기 -> 모든 페이지 대상
+    # save html code as a file
+    # name 변수 정규식으로 걸러내기
+    name = url.replace('https://','').replace('http://','').replace('/', '').replace('\\', '').replace('?', '').replace(':', '').replace('|', '').replace('*', '').replace('"', '').replace('<', '').replace('>', '')
+    #extract = tld.extract(url)
+    #path = "{}.{}".format(extract.domain, extract.suffix)
+    path = os.getcwd() + '\\web\\' + name
+    with open(path, 'a+', encoding='utf8') as f:
+        f.write(url + '\n')
         f.write(res.text)
-        f.close()
-        print("FILE CREATE: SUCCESS", path)
-        return
+    print("Web Parse Success -> ", url)
+    return
 
-    def get_url(url, res):
-        # extract new domain in a website
-        disasm_url = tld.extract(url)
-        soup = BeautifulSoup(res.text, 'html.parser')
-        with open('new.txt', 'a+') as f:
+def get_url(url, res):
+    # extract new domain in a website
+    disasm_url = tld.extract(url)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    with open('new.txt', 'a+') as f:
+        with open('total_url.txt', 'a+') as p:
             for a in soup.find_all('a', href=True):
                 if int('.onion' in a['href'])  & int('http' in a['href']): #& int(disasm_url.domain not in a['href'])
                     f.write(a['href'] + "\n")
-            f.close()
-
-        return
+                    p.write(a['href'] + "\n")
+    return
 
     #def find_url(path):
 
+def run(url_file):
+    i = 0
+
+    while(1):
+        print('URL 수집 시작 {}회\t'.format(i), datetime.datetime.now())
+        #url 배열로 받아오기
+        with open(url_file, 'r+') as f:
+            urls = f.readlines()
+
+        for i in range(len(urls)):
+            url = urls[i].replace('\n', '')
+            #url 응답 보내기
+            try:
+                res = requests.get(url, proxies=proxies, timeout=10)
+            except Exception as e:
+                print("Request Timeout   -> ", url)
+                with open("pass.txt", 'a+') as p:
+                    p.write(url + '\n')
+                continue
+
+            #정상 응답일 경우 해당 url의 html code 및 추가 url 파싱
+            if res.status_code == 200:
+                web_parser(url, res)
+                get_url(url, res)
+            else:
+            #200이 아닐 경우 bat_status.txt에 url과 응답 번호 저장
+                print("response status code is not 200: {} -> {}".format(url, res.status_code))
+                with open('bad_status.txt', 'a+') as badurl:
+                    badurl.write("{} -> {}".format(url, res.status_code))
+
+        #새로운 url 저장 마쳤으면 기존 url 텍스트 파일 내용 비우기
+        with open('url.txt', 'w') as old:
+            print('url.txt 내용 비우기 완료')
+            with open('new.txt', 'r') as new:
+                old.write(new.read())
+                print('new.txt에 새로운 url 작성 완료')
+
+        open('new.txt', 'w')
+        print('new.txt 내용 비우기 완료')
+        print("URL 수집 완료\t", datetime.datetime.now())
+
+    return
 
 if __name__=="__main__":
-    #url = ['http://zqktlwi4fecvo6ri.onion/wiki/index.php/Main_Page', 'http://thehiddenwiki.org/']
-    with open('url.txt', 'r+') as f:
-        urls = f.readlines()
-
-    for i in range(len(urls)):
-        url = urls[i].replace('\n','')
-
-        try:
-            res = requests.get(url, proxies=proxies, timeout=10)
-        except:
-            print("EXCEPT PASS \t", url)
-            with open("pass.txt", 'a+') as p:
-                p.write(url + '\n')
-            continue
-
-        if res.status_code == 200:
-            crawler.web_parser(url, res)
-            crawler.get_url(url, res)
-        else:
-            print(url,"'s response: ", res.status_code)
-
-'''
-    url = 'http://thehiddenwiki.org/'
-
-    res = requests.get(url, proxies=proxies, timeout=10)
-    crawler.get_url(url,res)
-'''
-'''
-    for i in range(len(url)):
-        res = crawler.isurl_exist(url[i])
-
-        if res.status_code == 200:
-            crawler.web_parser(url[i], res)
-            crawler.get_url(url[i], res)
-
-'''
-
-
-'''
-    for a in soup.find_all('a', href=True):
-        url = a['href']
-        if int('.onion' in url) & int(disasm_url.domain not in url) & int('http' in url) :
-            
-'''
+    url_file = 'url.txt'
+    run(url_file)
