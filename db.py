db71189139288aa432805a807574dc166cc9abe7
teaf1001 <teaf1001@naver.com>
2021-01-26 17:06:07 +0900

    update

--- db.py
+++ db.py
@@ -1,100 +1,169 @@
 from pymongo import MongoClient
 import tldextract as tld
+import datetime
+DB_Client = MongoClient("mongodb://localhost:27017/")
+DB = DB_Client["DBtest100"]
 
-my_client = MongoClient("mongodb://localhost:27017/")
-DB = my_client["DBtest2"]
-collection_domain_info = DB['domain_info']
-collection_domains = DB['domains']
-collection_badurl = DB['badurl']
-collection_error = DB['error_log']
+#collection_domain_info = DB['domain_info'] # url, domain, sub_domain
+#collection_domains = DB['domains'] # domain
+#collection_badurl = DB['badurl'] # url, cause
+#collection_error = DB['error_log'] # domain, error code, url
+#collection_timeout = DB['timeout'] # domain, url
 
-class domain():
+
+class domain:
+
     def __init__(self):
-        self.domain = ''
+        self.url = ''
+        self.cause = ''
+        extract = tld.extract(self.url)
 
-    def insert_domains(self):
+    def insert_domains(self, url):
+        self.url = url
+        extract = tld.extract(url)
+        collection = DB['domains']
+        post = {
+            "domain": extract.domain
+        }
+        collection.insert_one(post)
+
+    def insert_domain_info(self, url):
+        self.url = url
+        extract = tld.extract(url)
+        collection = DB["domain_info"]
+        if extract.subdomain !='':
+            post = {
+                "url": url,
+                "domain": extract.domain,
+                "sub_domain": extract.subdomain,
+                "label": 0
+            }
+        else:
+            post = {
+                "url": url,
+                "domain": extract.domain,
+                "label": 0
+            }
+        collection.insert_one(post)
+
+    def insert_bad_url(self, url, cause):
+        self.url = url
+        self.cause = cause
+        collection = DB["bad_url"]
+        post = {
+            "url": url,
+            'cause': cause
+        }
+        collection.insert_one(post)
+
+    def insert_status_code(self, url, status_code):
+        self.url = url
+        collection = DB["status_code"]
+        post = {
+            "url" : url,
+            "response_code" : status_code
+        }
+        collection.insert_one(post)
+
+    def insert_error_log(self, url, exception):
+        self.url = url
+        collection = DB["error_log"]
+        post = {
+            "url" : url,
+            "error_code": str(exception)
+        }
+        collection.insert_one(post)
+
+    def insert_log(self, cnt):
+        collection = DB["log"]
+        post = {
+            "log": "URL 수집 시작 {}회".format(cnt),
+            "datetime": str(datetime.datetime.now())
+        }
+        collection.insert_one(post)
+
+    def is_exist_domain(self, url):
+        # Domains에 등록되어 있지 않음 -> domains와 domain_info에 둘다 추가
+        self.url = url
+        extract = tld.extract(url)
+
+        try:
+            if type(DB["domains"].find({"domain": extract.domain})[0]) == type(dict):
+                return True
+        except:
+            return False
+
+    def is_exist_url(self,url):
+        # Domain_info에 등록되어 있지 않음 -> domain_info에 추가
+        self.url = url
+        extract = tld.extract(url)
+
+        try:
+            if type(DB["domain_info"].find({"domain": extract.domain})[0]) == type(dict):
+                return True
+        except:
+            return False
+
+    def label_update(self, url):
+        self.url = url
+        domain= tld.extract(url).domain
+        DB['domain_info'].update_one({"domain": domain}, {"$set": {"label": 1}})
         return
 
-    def insert_domain_info(self):
-        return
 
-    def insert_bad_domain(self):
-        return
-
-# db.classic.find({ "product_name": { $exists: true }} ) 필드가 존재하는지 여부 확인
-
-# db.classic.findOne( { 'product_name': { $in: ['HHHHH'] }, 'product_category': { $in: ['TTTTTTTT'] } } )
-# -> $in을 사용
-
-# db.classic.findOne( { 'product_name': { $exists: true }, 'product_category': { $exists: true } } ) ->
-# $exists를 사용하여 필드가 레코드에 존재하는지 여부를 확인
-
-if __name__=="__main__":
+def run(url_file):
     dict = {}
-    with open("url.txt", "r+") as f:
+    with open(url_file, "r+") as f:
         urls = f.readlines()
         for i in range(len(urls)):
-            url = urls[i].replace("\n",'').replace('#','')
+            url = urls[i].replace("\n", '').replace('#', '')
             extract = tld.extract(url)
+
+            curl = domain()
 
             try:
                 if extract.suffix != 'onion':
                     cause = 'suffix error'
                     print('suffix is not .onion : ', url)
-                    post = {
-                        "url": url,
-                        'cause': cause
-                    }
-                    collection_badurl.insert(post)
-                    # bad_url에 insert
+                    curl.insert_bad_url(url, cause)
                     continue
 
                 if extract.domain == '':
                     cause = 'domain is not exist'
                     print('domain is not exist', url)
-                    post = {
-                        "url": url,
-                        'cause': cause
-                    }
-                    collection_badurl.insert(post)
-                    # bad_url에 insert
+                    curl.insert_bad_url(url, cause)
                     continue
 
-                if type(collection_domains.find_one({"domain": extract.domain})) == type(dict):
-                    post = {
-                        "url": url,
-                        "domain": extract.domain,
-                        "sub_domain": extract.subdomain
-                    }
-                    collection_domain_info.insert(post)
+                try:
+                    if type(DB["domains"].find({"domain": extract.domain})[0]) == type(dict):
+                        #domains에 등록되어 있는 지
+                        try:
+                            if type(DB["domain_info"].find({"domain": extract.domain})[0]) == type(dict):
+                                #domain_info에 등록되어 있음 -> continue
+                                continue
+                        except:
+                            #domain_info에 등록되어 있지 않음 -> domain_info에 등록
+                            curl.insert_domain_info(url)
 
-                else:
-                    post = {
-                        "domain": extract.domain
-                    }
-                    collection_domains.insert(post)
-
-                    post = {
-                        "url": url,
-                        "domain": extract.domain,
-                        "sub_domain": extract.subdomain
-                    }
-                    collection_domain_info.insert(post)
+                except:
+                    #Domains에 등록되어 있지 않음 -> domains와 domain_info에 둘다 추가
+                    curl.insert_domains(url)
+                    curl.insert_domain_info(url)
 
             except Exception as e:
+                #기타 에러
+                print('DB insert error:', e)
                 post = {
                     "url": url,
                     "exception": e
                 }
-                collection_error.insert()
+                curl.insert_error_log(url, e)
                 pass
 
-            # print(url)
-            # post = {"url" : url,"sub_domain" : extract.subdomain, "domain" : extract.domain}
-            # post = {"domain": extract.domain, "url": url}
-            # collection_domains.insert(post)
-
-
-            # print(extract)
-            # print("subdomain:{}, domain:{}, suffix:{}".format(extract.subdomain, extract.domain, extract.suffix))
-
+if __name__ == "__main__":
+    Cdomain = domain()
+    url = "http://wikitjerrta4qgz4.onion/"
+    Cdomain.insert_domain_info(url)
+    Cdomain.insert_domains(url)
+    #file = 'url.txt'
+    #run(file)
--- dd.py
+++ dd.py
@@ -4,23 +4,32 @@ from bs4 import BeautifulSoup
 import datetime
 import tldextract as tld
 import time
+from db import domain
+from pymongo import MongoClient
 
 proxies = {
     'http': 'socks5h://127.0.0.1:9150',
     'https': 'socks5h://127.0.0.1:9150'
 }
 
-numberof_url = 0
+DB_Client = MongoClient("mongodb://localhost:27017/")
+DB = DB_Client["DBtest100"]
 
 def web_parser(url, res):
     # 수정하기 : 파일 이름 도메인 + 서픽스로 하지 말고 http://만 빼고 다 작성하기 -> 모든 페이지 대상
     # save html code as a file
     # name 변수 정규식으로 걸러내기
-    #name = url.replace('https://','').replace('http://','').replace('/', '').replace('\\', '').replace('?', '').replace(':', '').replace('|', '').replace('*', '').replace('"', '').replace('<', '').replace('>', '')
-    extract = tld.extract(url)
-    name = "{}.{}".format(extract.domain, extract.suffix)
+    try:
+        name = url.replace('https://','').replace('http://','').replace('/', '').replace('\\', '').replace('?', '').replace(':', '').replace('|', '').replace('*', '').replace('"', '').replace('<', '').replace('>', '').replace('#','')
+    except Exception as e:
+        print('web_parser error :{}, url:'.format(e, url))
+        return
+
+    #extract = tld.extract(url)
+    #name = "{}.{}".format(extract.domain, extract.suffix)
+
     path = os.getcwd() + '\\web\\' + name
-    with open(path, 'a+', encoding='utf8') as f:
+    with open(path, 'w', encoding='utf8') as f:
         try:
             f.write(url + '\n')
             f.write(res.text)
@@ -29,83 +38,68 @@ def web_parser(url, res):
     print("Web Parse Success -> ", url)
     return
 
-def get_url(url, res):
-    global numberof_url
+def get_url(res, cDB):
     # extract new domain in a website
-    disasm_url = tld.extract(url)
     soup = BeautifulSoup(res.text, 'html.parser')
-    with open('new.txt', 'a+') as f:
-        with open('total_url.txt', 'a+') as p:
-            for a in soup.find_all('a', href=True):
-                if int('.onion' in a['href']) & int('http' in a['href']) & int(disasm_url.domain not in a['href']): #
-                    f.write(a['href'] + "\n")
-                    p.write(a['href'] + "\n")
-                    numberof_url += 1
+    for a in soup.find_all('a', href=True):
+
+        if int('.onion' in a['href']) & int('http' in a['href']):
+            if cDB.is_exist_domain(a['href']):
+                if cDB.is_exist_url([a['href']]):
+                    continue
+                else:
+                    cDB.insert_domain_info(a['href'])
+
+            else:
+                cDB.insert_domains(a['href'])
+                cDB.insert_domain_info(a['href'])
     return
 
 #def find_url(path):
 
-def run(url_file):
-    cnt = 0
-    total = 0
+def run():
+    cDB = domain()
+
+    WholeCnt = 0
     while(1):
-        global numberof_url
-        numberof_url = 0
-        parsed = 0
 
-        log = 'URL 수집 시작 {}회\t {} \n'.format(cnt, datetime.datetime.now())
+        log = 'URL 수집 시작 {}회\t {} \n'.format(WholeCnt, datetime.datetime.now())
         print(log)
+        cDB.insert_log(log)
 
-        with open('log.txt', 'a+') as log_txt:
-            log_txt.write(log)
+        urllist = []
+        for url in DB['domain_info'].find({"label": 0}, {"_id": 0, "url": 1}):
+            raw = list(url.values())[0]
+            urllist.append(raw)
 
-        #url 배열로 받아오기
-        with open(url_file, 'r+') as f:
-            urls = f.readlines()
-
-        for i in range(len(urls)):
-            url = urls[i].replace('\n', '')
-            #url 응답 보내기
+        for url in urllist:
+            #url get 요청 보내기
             try:
                 res = requests.get(url, proxies=proxies, timeout=10)
             except Exception as e:
                 print("Request Timeout   -> ", url)
-                with open("pass.txt", 'a+') as p:
-                    p.write(url + '\n')
+                cDB.label_update(url)
+                cDB.insert_bad_url(url, "Timeout")
                 continue
 
             #정상 응답일 경우 해당 url의 html code 및 추가 url 파싱
             if res.status_code == 200:
                 web_parser(url, res)
-                parsed += 1
-                get_url(url, res)
+                get_url(res, cDB)
+                cDB.label_update(url)
+                continue
+
             else:
             #200이 아닐 경우 bat_status.txt에 url과 응답 번호 저장
                 print("response status code is not 200: {} -> {}".format(url, res.status_code))
-                with open('bad_status.txt', 'a+') as badurl:
-                    badurl.write("{} -> {}\n".format(url, res.status_code))
+                cDB.insert_status_code(url, res.status_code)
+                cDB.label_update(url)
+                continue
 
-        #새로운 url 저장 마쳤으면 기존 url 텍스트 파일 내용 비우기
-        with open('url.txt', 'w') as old:
-            print('url.txt 내용 비우기 완료')
-            with open('new.txt', 'r') as new:
-                old.write(new.read())
-                print('new.txt에 새로운 url 작성 완료')
-
-        open('new.txt', 'w')
-        print('new.txt 내용 비우기 완료')
         print("URL 수집 완료\t", datetime.datetime.now())
+        WholeCnt += 1
 
-        with open('log.txt', 'a+') as log_txt:
-            log = 'URL {}개 파싱\n새 도메인 {}개 등록 완료\n\n'.format(parsed, numberof_url)
-            total += numberof_url
-            print(log)
-            print("수집한 총 url: {}개".format(total))
-            log_txt.write(log)
-
-        cnt += 1
     return
 
 if __name__=="__main__":
-    url_file = 'url.txt'
-    run(url_file)
+    run()
