738a8635232bf74c97febf173a5d6e98625852d8
teaf1001 <teaf1001@naver.com>
2021-01-28 11:46:19 +0900

    Error Fix

--- db.py
+++ db.py
@@ -7,97 +7,91 @@ class DB_Handler:
     def __init__(self):
         self.cause = ''
         self.DB_Client = MongoClient("mongodb://localhost:27017/")
-        self.DB = self.DB_Client["d41"]
+        self.DB = self.DB_Client["d106"]
 
     def insert_domains(self, url):
         extract = tld.extract(url)
-        collection = self.DB['domains']
+        collection = self.DB['Domains']
         post = {
-            "domain": extract.domain
+            "Domain": extract.domain
         }
         collection.insert_one(post)
 
     def insert_domain_info(self, url):
         extract = tld.extract(url)
-        collection = self.DB["domain_info"]
+        collection = self.DB["Domain_Info"]
         if extract.subdomain !='':
             post = {
-                "url": url,
-                "domain": extract.domain,
-                "sub_domain": extract.subdomain,
-                "label": 0
+                "Url": url,
+                "Domain": extract.domain,
+                "Sub_domain": extract.subdomain,
+                "Label": 0
             }
         else:
             post = {
-                "url": url,
-                "domain": extract.domain,
-                "label": 0
+                "Url": url,
+                "Domain": extract.domain,
+                "Label": 0
             }
         collection.insert_one(post)
 
     def insert_bad_url(self, url, cause, status_code):
         self.cause = cause
-        collection = self.DB["bad_url"]
-        if cause == 'status_code':
+        collection = self.DB["Bad_Url"]
+        if cause == 'Status_Code':
             post = {
-                "url": url,
-                'cause': cause
+                "Url": url,
+                'Cause': cause,
+                'Status_Code': status_code
             }
             collection.insert_one(post)
             self.label_update(url)
         else:
             post = {
-                "url": url,
-                'cause': cause,
-                'status_code': status_code
+                "Url": url,
+                'Cause': cause,
             }
             collection.insert_one(post)
             self.label_update(url)
 
-
-    def insert_log(self, cnt):
-        collection = self.DB["log"]
-        post = {
-            "log": "URL 수집 시작",
-            "count": cnt,
-            "datetime": str(datetime.datetime.now())
-        }
-        collection.insert_one(post)
-
-    def is_exist_domain(self, url):
-        extract = tld.extract(url)
-        if str(self.DB["domains"].find_one({"domain": extract.domain})) != "None":
-            return True
+    def insert_log(self, cnt, new_url_tuple):
+        collection = self.DB["Log"]
+        if cnt != -1:
+            post = {
+                "Log": "URL 수집 시작: {}회".format(cnt),
+                "Datetime": str(datetime.datetime.now()),
+            }
+            collection.insert_one(post)
         else:
-            return False
+            post = {
+                "Log": "URL 수집 끝",
+                "Datetime": str(datetime.datetime.now()),
+                "New_domain": new_url_tuple[0],
+                "New_url": new_url_tuple[1]
+            }
+            collection.insert_one(post)
 
-    def is_exist_url(self, url):
-        collection = self.DB['domain_info']
-        if str(collection.find_one({"url": url})) != "None":
-            return True
+    def is_exist_url(self, url, collection):
+
+        if collection == 'Domains':
+            collection = self.DB[collection]
+            extract = tld.extract(url)
+            if str(collection.find_one({"Domain": extract.domain})) != "None":
+                return True
+            else:
+                return False
+
+        elif (collection == 'Domain_Info') or (collection == 'Bad_Url'):
+            collection = self.DB[collection]
+            if str(collection.find_one({"Url": url})) != "None":
+                return True
+            else:
+                return False
         else:
-            return False
+            print("is_exist_url Unexpected Result-: Url:{}".format(url), collection)
+            return
+
 
     def label_update(self, url):
-        self.DB['domain_info'].update_many({"url": url}, {"$set": {"label": 1}})
+        self.DB['Domain_Info'].update_many({"Url": url}, {"$set": {"Label": 1}})
         return
-
-
-def run(url_file):
-    dict = {}
-    with open(url_file, "r+") as f:
-        urls = f.readlines()
-        for i in range(len(urls)):
-            url = urls[i].replace("\n", '').replace('#', '')
-            extract = tld.extract(url)
-            curl = DB_Handler()
-
-
-
-if __name__ == "__main__":
-    hDB = DB_Handler()
-    url = "http://dirnxxdraygbifgc.onion/"
-    hDB.insert_domain_info(url)
-    hDB.insert_domains(url)
-    #file = 'url.txt'
-    #run(file)
--- dd.py
+++ dd.py
@@ -3,9 +3,7 @@ import os
 from bs4 import BeautifulSoup
 import datetime
 import tldextract as tld
-import time
 from db import DB_Handler
-from pymongo import MongoClient
 
 proxies = {
     'http': 'socks5h://127.0.0.1:9150',
@@ -28,79 +26,109 @@ def web_parser(url, res, hDB):
             f.write(res.text)
         except Exception as e:
             print("FILE CREATE ERROR -> ", e)
-
             return
-    print("Web Parse Success -> ", url)
+
+    #print("Web Parse Success -> ", url)
     return
 
 def get_url(res, hDB):
+    new_url = 0
+    new_domain = 0
     # extract new domain in a website
     soup = BeautifulSoup(res.text, 'html.parser')
     for a in soup.find_all('a', href=True):
         lnk = a['href']
-        if lnk[-1] != '/':
-            lnk = lnk + '/'
+        lnk = lnk.replace(' ','')
 
-        if int('.onion' in lnk) & int('http' in lnk):
-            if hDB.is_exist_domain(lnk):
-                if hDB.is_exist_url(lnk):
+        #예기치 못한 에러 점검
+        try:
+            if len(lnk) == 0 :
+                continue
+            else:
+                if lnk[-1] != '/':
+                    lnk = lnk + '/'
+        except Exception as e:
+            print("Error in get_url -> Url: ", lnk, e)
+            pass
+        #서픽스, 프로토콜 검사
+        if (tld.extract(lnk).suffix == 'onion') and ('http' in lnk):
+            if hDB.is_exist_url(lnk, 'Domains'):
+                if hDB.is_exist_url(lnk, 'Domain_Info'):
                     continue
                 else:
                     #print("insert domain_info : {}".format(lnk))
                     hDB.insert_domain_info(lnk)
+                    new_url += 1
             else:
                 #print("insert domains/domain_info :{}".format(lnk))
                 hDB.insert_domains(lnk)
                 hDB.insert_domain_info(lnk)
-    return
+                new_domain += 1
+                new_url += 1
+    #print("get_url:{},{}".format(new_domain,new_url))
+    return new_domain, new_url
 
 def run():
     hDB = DB_Handler()
+    print(hDB.DB)
 
-    WholeCnt = 0
+    WholeCnt = 1
     while(1):
-        log = 'URL 수집 시작 {}회\t {} \n'.format(WholeCnt, datetime.datetime.now())
+        log = 'URL 수집 {}회 시작\t\t\t\t\t{}'.format(WholeCnt, datetime.datetime.now())
         print(log)
-        hDB.insert_log(WholeCnt+1)
+        hDB.insert_log(WholeCnt, (0,0))
+
+        _new_url = [0, 0]
 
         urllist = []
-        for url in hDB.DB['domain_info'].find({"label": 0}, {"_id": 0, "url": 1}):
+        #DB에서 파싱 안한 URL 가져오기
+        for url in hDB.DB['Domain_Info'].find({"Label": 0}, {"_id": 0, "Url": 1}):
             raw = list(url.values())[0]
             urllist.append(raw)
 
         for url in urllist:
             #print("len of domain : {}".format(len(tld.extract(url).domain)))
-            url = url.replace("http//", "")
+            url = url.replace("http//", "").replace("#",'')
 
-            #한번 더 테스트
+            #서픽스 검사
+            if tld.extract(url).suffix != 'onion':
+                hDB.insert_bad_url(url, "Suffix Error", 0)
+                continue
+            #혹시 모를 디버깅
             if (len(tld.extract(url).domain) != 16) and (len(tld.extract(url).domain) != 56):
-                hDB.insert_bad_url(url, "DomainError", 1)
-                print("domain error", len(tld.extract(url).domain) , tld.extract(url).domain)
+                hDB.insert_bad_url(url, "Domain Error", 1)
+                print("Domain Error", len(tld.extract(url).domain) , tld.extract(url).domain, url)
+                continue
+            #중복 검사
+            if hDB.is_exist_url(url, 'Bad_Url'):
+                continue
 
-            #url get 요청 보내기
+            #url에 get 요청 보내기
             try:
                 res = requests.get(url, proxies=proxies, timeout=8)
             except Exception as e:
-                print("Request Timeout   -> ", url)
+                #print("Request Timeout   -> ", url)
                 hDB.insert_bad_url(url, "Timeout", 2)
                 continue
-
             #정상 응답일 경우 해당 url의 html code 및 추가 url 파싱
             if res.status_code == 200:
                 web_parser(url, res, hDB)
-                get_url(res, hDB)
+                nurl = list(get_url(res, hDB))
                 hDB.label_update(url)
+                _new_url[0] += nurl[0]
+                _new_url[1] += nurl[1]
             else:
             #200이 아닐 경우 bat_status.txt에 url과 응답 번호 저장
-                print("status code is not 200: {} -> {}".format(url, res.status_code))
-                hDB.insert_bad_url(url, "status_code", res.status_code)
+                #print("Status_Code is not 200: {} -> {}".format(url, res.status_code))
+                hDB.insert_bad_url(url, "Status_Code", res.status_code)
 
-        print("URL 수집 완료\t", datetime.datetime.now())
+        print("URL 수집 {}회 완료\t domain:{}\t url:{}\t{}".format(WholeCnt,_new_url[0],_new_url[1],datetime.datetime.now()))
+
+        hDB.insert_log(-1, _new_url)
         WholeCnt += 1
-
     return
 
-if __name__=="__main__":
+if __name__ == "__main__":
     hDB = DB_Handler()
     url = "http://dirnxxdraygbifgc.onion/"
     hDB.insert_domain_info(url)
